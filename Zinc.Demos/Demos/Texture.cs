baaaf45451eb2d1f8d03251c22b0f6f3c3e6e4e3
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2024-06-27 20:10:45 -0400

    getting props setup

--- Zinc.Demos/Demos/Texture.cs
+++ Zinc.Demos/Demos/Texture.cs
@@ -5,11 +5,11 @@ public class Texture : Scene
 {
     public override void Create()
     {
-        // var tex = Res.Assets.conscript.CreateSprite();
-        // tex.X = Engine.Width / 2f;
-        // tex.Y = Engine.Height / 2f;
-        // tex.PivotX = 256;
-        // tex.PivotY = 256;
+        var tex = Res.Assets.conscript.CreateSprite();
+        tex.X = Engine.Width / 2f;
+        tex.Y = Engine.Height / 2f;
+        tex.PivotX = 256;
+        tex.PivotY = 256;
         
         // foreach (var line in Depot.Generated.depot.testSheet.Lines)
         // {
