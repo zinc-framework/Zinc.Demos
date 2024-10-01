e0a5cfda2ad26e0454875fa3e409ef7af9230080
Kyle Kukshtel <kyle.kukshtel@gmail.com>
2024-10-01 15:01:34 -0400

    update particle system code

--- Zinc.Demos/Demos/ParticleSystem.cs
+++ Zinc.Demos/Demos/ParticleSystem.cs
@@ -1,4 +1,5 @@
 using Zinc.Core;
+using Zinc.Core.ImGUI;
 using static Zinc.Quick;
 
 namespace Zinc.Sandbox.Demos;
@@ -11,23 +12,23 @@ public class ParticleSystem : Scene
     private Color endColor = new Color(1.0f, 0.0f, 1.0f, 0f);
     // Color endColor = Palettes.ENDESGA[16];
     ParticleEmitter emitter;
+    ParticleEmitterConfig config = ParticleEmitterConfig.DefaultConfig;
     public override void Create()
     {
-        emitter = new ParticleEmitter(
-            new(100000, 100, new ParticleEmitterComponent.ParticleConfig()
-            {
-                Color = new Transition<Color>(startColor,endColor,Easing.Option.EaseInOutExpo)
-            }))
-        {
-            X = 200,
-            Y = 200
+        emitter = new ParticleEmitter(100,config){
+            X = 400,
+            Y = 400,
         };
     }
 
     public override void Update(double time)
     {
-        MoveToMouse(emitter);
-        DrawEditGUIForObject("emitter",ref emitter);
+        // emitter.Emitter_Config = emitter.Emitter_Config with {EmissionRate = 0.1f};
+        // MoveToMouse(emitter);
+        var rot = emitter.Rotation;
+        DrawEditGUIForObject("emitter",ref config);
+        ImGUIHelper.Wrappers.SliderFloat("Rotation", ref rot, 0, 360,"",Internal.Sokol.ImGuiSliderFlags_.ImGuiSliderFlags_None);
+        emitter.Rotation = rot;
         // var rand = RandUnitCircle();
         // emitter.Config.particleConfig.DX.StartValue = rand.x * 4;
         // emitter.Config.particleConfig.DY.StartValue = rand.y * 4;
